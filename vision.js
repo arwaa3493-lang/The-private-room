f9f77e1e42a95d7bd6c167d4637ace664b6d6995
arwaa arebee <arwaa3493@gmail.com>
2026-03-09 19:15:24 +0200

    Update vision.js

--- vision.js
+++ vision.js
@@ -34,4 +34,26 @@ cardMappings.forEach(({ inputId, imgId }) => {
   const inputEl = document.getElementById(inputId);
   const imgEl = document.getElementById(imgId);
   if (inputEl && imgEl) handleImageUpload(inputEl, imgEl);
+const inputs = document.querySelectorAll("input[type='file']");
+
+inputs.forEach(input => {
+
+input.addEventListener("change", function(){
+
+const file = this.files[0];
+if(!file) return;
+
+const previewId = this.dataset.preview;
+const img = document.getElementById(previewId);
+
+const reader = new FileReader();
+
+reader.onload = function(e){
+img.src = e.target.result;
+};
+
+reader.readAsDataURL(file);
+
+});
+
 });
